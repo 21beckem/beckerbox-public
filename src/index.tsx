8f16753e7c384eb4de8e2a91a52e07db3474708a
Michael Becker <21beckem8@gmail.com>
2026-04-10 20:07:09 -0600

    Fixed build error

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,2 @@
-/* @refresh reload */
-import { render } from 'solid-js/web'
-import './index.css'
-import App from './App.tsx'
-
-const root = document.getElementById('root')
-
-render(() => <App />, root!)
+// redirect to newest remote
+location.href = '/v2.2/';
